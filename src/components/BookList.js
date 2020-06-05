b48a12868867523bec586fb23408eb16c866da16
Karan Kumar <nailedITnerd@gmail.com>
2020-06-05 17:34:50 +0530

    Using ContextConsumer

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,19 +1,26 @@
 import React, {Component} from 'react';
 import {ThemeContext} from '../contexts/ThemeContext';
 
+/*****************************
+ * using ThemeContext.Consumer
+ */
 class BookList extends Component {
-    static contextType = ThemeContext;
     render() {
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
         return(
+        <ThemeContext.Consumer>{(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return(
             <div className="book-list" style={{background: theme.bg, color: theme.text}}>
-                <ul>
-                    <li style={{background: theme.ui}}>Book1</li>
-                    <li style={{background: theme.ui}}>Book2</li>
-                    <li style={{background: theme.ui}}>Book3</li>
-                </ul>
+            <ul>
+                <li style={{background: theme.ui}}>Book1</li>
+                <li style={{background: theme.ui}}>Book2</li>
+                <li style={{background: theme.ui}}>Book3</li>
+            </ul>
             </div>
+          )
+        }}</ThemeContext.Consumer>
+            
         )
     }
 }
